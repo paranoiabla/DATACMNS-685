3706fb44d3f55e43284328b2aaeb6c4b2b03a73e
Petar Tahchiev <paranoiabla@gmail.com>
2015-05-24 14:08:44 +0300

    Added a failing test

--- src/test/java/demo/DemoApplicationTests.java
+++ src/test/java/demo/DemoApplicationTests.java
@@ -1,16 +1,44 @@
 package demo;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import javax.annotation.Resource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private final MediaType MEDIA_TYPE = MediaType.parseMediaType("application/hal+json;charset=UTF-8");
+
+    private MockMvc mockMvc;
+
+    @Resource
+    private WebApplicationContext wac;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testQueryMethodIsSuccessfull() throws Exception {
+        mockMvc.perform(get("/productModels/search/findByCategoryEquals").param("category", "123").accept(MEDIA_TYPE).contentType(MEDIA_TYPE)).
+                        andExpect(status().isOk()).andExpect(content().contentType(MEDIA_TYPE));
+
+    }
 
 }
